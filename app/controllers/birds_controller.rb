b679cd99cb6a5ce99203cd644ec5bb40e6bb6d62
Ahmad Shah <ah.seerat@gmail.com>
2022-11-14 14:04:34 -0500

    commit tree 12c2e79886fe8d84b04102f4293dcbfd1ea5fcec

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,14 +1,21 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    # render json: @birds, only: [:id, :name, :species]
-    # render json: @birds, except: [:created_at, :updated_at]
-    render json: @birds.to_json(except: [:created_at, :updated_at])
+    birds = Bird.all
+    # render json: birds, only: [:id, :name, :species]
+    # render json: birds, except: [:created_at, :updated_at]
+    render json: birds.to_json(except: [:created_at, :updated_at])
+    # render json: birds, except: [:created_at, :updated_at]
+
   end
 
-  def show 
+  def show
     bird = Bird.find_by(id: params[:id])
-    # render json: {id: bird.id, name: bird.name, species: bird.species }
-    render json: bird.slice(:id, :name, :species)
+    if bird
+    render json: {id: bird.id, name: bird.name, species: bird.species}
+    else  
+   render json: {TEENGTAAANG: " Sorry no birth by this entery, try a diff number"}  
+end
+    # render json: bird.slice(:id, :name, :species)
   end
 end
+
